e73b40fa710ce59e217ef857d0b030c72295fb9a
Nathan Fox <nathan@merizer.com>
2016-09-25 21:34:17 -0400

    Update project to compile against Elixir 1.3.1. Changed mix.exs to allow elixir <= 1.4.0.

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Extreme.Mixfile do
   def project do
     [app: :extreme,
      version: "0.5.1",
-     elixir: ">= 1.0.0 and < 1.3.0",
+     elixir: ">= 1.0.0 and <= 1.4.0",
      source_url: "https://github.com/exponentially/extreme",
      description: """
      Elixir TCP adapter for EventStore.
